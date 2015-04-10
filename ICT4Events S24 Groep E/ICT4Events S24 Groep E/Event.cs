b560640e2d086f3ac26198fd44ef1780f765245c
Sven Henderickx <henderickx.sven@gmail.com>
2015-04-10 19:21:47 +0200

    Event classe

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Event.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events
+{
+    class Event
+    {
+        //Fields
+        private string naam;
+        private DateTime beginDatum;
+        private DateTime eindDatum;
+        private string plaats;
+        private string adres;
+        //private Beheerder!
+
+        //Properties
+        public string Naam
+        {
+            get { return naam; }
+        }
+
+        public DateTime BeginDatum
+        {
+            get { return beginDatum; }
+        }
+
+        public DateTime EindDatum
+        {
+            get { return eindDatum; }
+        }
+
+        public string Plaats
+        {
+            get { return plaats; }
+        }
+
+        public string Adres
+        {
+            get { return adres; }
+        }
+
+        //Constructors
+        public Event(string naam, DateTime beginDatum, DateTime eindDatum, string plaats, string adres)
+        {
+            this.naam = naam;
+            this.beginDatum = beginDatum;
+            this.eindDatum = eindDatum;
+            this.plaats = plaats;
+            this.adres = adres;
+        }
+
+        //Methodes
+
+    }
+}
