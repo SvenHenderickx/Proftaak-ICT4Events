af5d659328882f80a10a352ade5b0ec392c499ab
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-14 11:01:32 +0200

    Class Bericht Aangemaakt

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Bericht.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_S24_Groep_E
+{
+    class Bericht
+    {
+        private int likes;
+        private int aantalKeerGerapporteerd;
+        private int aantalKeerBekeken;
+        private string tekst;
+
+        public int Likes
+        {
+            get { return likes; }
+        }
+
+        public int AantalKeerGerapporteerd
+        {
+            get { return aantalKeerGerapporteerd; }
+        }
+
+        public int AantalKeerBekeken
+        {
+            get { return aantalKeerBekeken; }
+        }
+
+        public string Tekst
+        {
+            get { return tekst; }
+        }
+
+        public Bericht(string tekst)
+        {
+            this.tekst = tekst;
+        }
+    }
+}
