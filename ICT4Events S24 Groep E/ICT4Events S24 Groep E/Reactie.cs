1029aaecc743d9f6d1ecb447eee3062838b16f63
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-14 11:05:55 +0200

    Class Reactie Aangemaakt

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Reactie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_S24_Groep_E
+{
+    class Reactie
+    {
+        private int reactienummer;
+        private Bezoeker bezoeker;
+        private string inhoud;
+        private bool gerapporteerd;
+
+        public int Reactienummer
+        {
+            get { return reactienummer; }
+        }
+
+        public Bezoeker Bezoeker
+        {
+            get { return bezoeker; }
+        }
+
+        public string Inhoud
+        {
+            get {return inhoud;}
+        }
+
+        public bool Gerapporteerd
+        {
+            get { return gerapporteerd; }
+        }
+
+        public Reactie(Bezoeker bezoeker, string inhoud)
+        {
+            this.inhoud = inhoud;
+        }
+    }
+}
