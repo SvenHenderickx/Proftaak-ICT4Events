96095d42f10c8c9ccdd0665abbeeec894ece2268
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-14 13:22:26 +0200

    Class Reservering aangemaakt

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Reservering.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_S24_Groep_E
+{
+    class Reservering
+    {
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+        }
+
+        public Reservering(Hoofdboeker hoofdboeker)
+        {
+
+        }
+
+        public bool HuurMateriaal(Huuritem huuritem)
+        {
+            // Als het huuritem nog niet gehuurd is, moet deze bij een persoon aan de lijst hehuurd toegevoegd worden.
+            // bool isgehuurd wordt true.
+            
+        }
+    }
+}
