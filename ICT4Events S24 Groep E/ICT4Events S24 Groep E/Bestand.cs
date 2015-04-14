ea616290468a1f0db04c0f807e980bf04da11c39
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-14 11:14:01 +0200

    Class Bestand Aantgemaakt

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Bestand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_S24_Groep_E
+{
+    class Bestand
+    {
+        private string naam;
+        private Persoon maker;
+        private int likes;
+        private string pad;
+        private int aantalKeerGerapporteerd;
+        private int aantalKeerBekeken;
+
+        public string Naam
+        {
+            get { return naam; }
+        }
+
+        public Persoon Maker
+        {
+            get { return maker; }
+        }
+
+        public int Likes
+        {
+            get { return likes; }
+        }
+
+        public string Pad
+        {
+            get { return pad; }
+        }
+
+        public int AantalKeerGerapporteerd
+        {
+            get { return aantalKeerGerapporteerd; }
+        }
+
+        public int AantalKeerBekeken
+        {
+            get { return aantalKeerBekeken; }
+        }
+
+        public Bestand(Bezoeker bezoeker, Categorie categorie, string naam, string pad)
+        {
+            this.naam = naam;
+            this.pad = pad;
+        }
+
+        public bool VoegReactieToe()
+        {
+
+        }
+
+        public void VerwijderReactie(int reactienummer)
+        {
+
+        }
+    }
+}
