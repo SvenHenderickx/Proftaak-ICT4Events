fc5de098807038ed6dfd0581399a6a9b8fb98756
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-14 10:56:58 +0200

    Class Categorie Aangemaakt

---
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/Categorie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Events_S24_Groep_E
+{
+    class Categorie
+    {
+        private string naam;
+
+        public string Naam
+        {
+            get { return naam; }
+        }
+
+        public Categorie(Bezoeker bezoeker, string naam)
+        {
+            this.naam = naam;
+        }
+
+        public bool VoegBestandToe()
+        {
+
+        }
+    }
+}
