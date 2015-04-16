3bd9070c4923df3346e790bf0cdcc7990a7b9141
AwesomeKevin <k.kopp@student.fontys.nl>
2015-04-16 11:33:52 +0200

    niks

--- ICT4Events S24 Groep E/ICT4Events S24 Groep E/EventBeheerForm.Designer.cs
+++ ICT4Events S24 Groep E/ICT4Events S24 Groep E/EventBeheerForm.Designer.cs
@@ -38,6 +38,14 @@
             this.lbGebruikerinfo = new System.Windows.Forms.ListBox();
             this.btnInfoOpvraag = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.btnDatabaseConnectie = new System.Windows.Forms.Button();
             this.cbCategorieSelecter = new System.Windows.Forms.ComboBox();
             this.cbMateriaalSelecter = new System.Windows.Forms.ComboBox();
@@ -74,14 +82,6 @@
             this.eventTab = new System.Windows.Forms.TabPage();
             this.gebruikerTab = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
             this.gbGebruikers.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -217,6 +217,78 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "EventBeheer";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(385, 225);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(68, 23);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Verwijder";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(385, 176);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Verwijder";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(696, 225);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(67, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "voeg toe";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(385, 202);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(68, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Verwijder";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(696, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "voeg toe";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(696, 202);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "voeg toe";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Verwijder";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "voeg toe";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // btnDatabaseConnectie
             // 
@@ -534,83 +606,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnMaakEventAan_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(696, 153);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(67, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "voeg toe";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(385, 153);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(68, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Verwijder";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(696, 176);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(67, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "voeg toe";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(385, 176);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(68, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Verwijder";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(696, 202);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(67, 23);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "voeg toe";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(385, 202);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(68, 23);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Verwijder";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(696, 225);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(67, 23);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "voeg toe";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(385, 225);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(68, 23);
-            this.button9.TabIndex = 7;
-            this.button9.Text = "Verwijder";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
             // EventBeheerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 418);
+            this.ClientSize = new System.Drawing.Size(812, 452);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
